c460adb3504cb0ef7519a3953a1a3e0c436882d6
hridhya <menonhridhya@gmail.com>
2015-08-05 15:28:12 -0700

    chapter 10

--- chapter10_1.py
+++ chapter10_1.py
@@ -1,1 +1,24 @@
-print 'Hello'
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+def count(lines,s):
+    counts = dict()
+    for line in lines:
+        if line.startswith(s) and not line.startswith(s+':'):
+            line = ((line.rstrip()).lstrip()).split()
+	    str = line[5]
+	    hour = str[0:str.find(":"):1]
+            counts[hour] = counts.get(hour,0) + 1
+    return counts
+
+def sort(d):
+    lst = list()
+    for key, val in d.items():
+        lst.append((key,val))
+    lst.sort()
+    for val,key in lst:
+        print val,key
+    
+dictionary = count(handle,'From')
+t = sort(dictionary)
